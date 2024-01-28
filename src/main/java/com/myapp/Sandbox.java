7e0e000c75aadd6fa84ffd1cc70abfc3e2c2d368
MykytaD <m.dubiaha887@gmail.com>
2024-01-28 18:45:44 +0200

    REQ-07: Enum dog size.

--- src/main/java/com/myapp/Sandbox.java
+++ src/main/java/com/myapp/Sandbox.java
@@ -1,9 +1,7 @@
 package main.java.com.myapp;
-
-import main.java.com.myapp.comparator.DogSizeComparator;
 import main.java.com.myapp.model.Dog;
-
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.function.DoubleFunction;
 
 public class Sandbox {
@@ -11,15 +9,22 @@ public class Sandbox {
     public static void main(String[] args) {
 
         Dog[] dogs = {
-                new Dog(Dog.M, "4"),
-                new Dog(Dog.S, "1"),
-                new Dog(Dog.XL, "6"),
-                new Dog(Dog.XL, "2"),
-                new Dog(Dog.XS, "3"),
-                new Dog(Dog.S, "5")
+                new Dog(Dog.Size.M, "4"),
+                new Dog(Dog.Size.S, "1"),
+                new Dog(Dog.Size.XL, "6"),
+                new Dog(Dog.Size.XL, "2"),
+                new Dog(Dog.Size.XS, "3"),
+                new Dog(Dog.Size.S, "5")
         };
 
-        Arrays.sort(dogs, new DogSizeComparator());
+        Arrays.sort(dogs, new Comparator<Dog>() {
+            @Override
+            public int compare(Dog o1, Dog o2) {
+                int dogSizeCompare = o1.getSize().getValue() - o2.getSize().getValue();
+                int dogAgeCompare = o1.getAge().compareTo(o2.getAge());
+                return (dogSizeCompare == 0) ? dogAgeCompare : dogSizeCompare;
+            }
+        });
 
         for (Dog dog : dogs) {
             System.out.println(dog.getSize() + "\t" + dog.getAge());
--- src/main/java/com/myapp/comparator/DogSizeComparator.java
+++ src/main/java/com/myapp/comparator/DogSizeComparator.java
@@ -1,26 +1,26 @@
-package main.java.com.myapp.comparator;
-
-import main.java.com.myapp.model.Dog;
-
-import java.util.Comparator;
-
-public class DogSizeComparator implements Comparator<Dog> {
-
-    @Override
-    public int compare(Dog dog1, Dog dog2) {
-        int dogSizeCompare = convertSize(dog1.getSize()) - convertSize(dog2.getSize());
-        int dogAgeCompare = dog1.getAge().compareTo(dog2.getAge());
-        return (dogSizeCompare == 0) ? dogAgeCompare : dogSizeCompare;
-    }
-
-    private static int convertSize(String size) {
-        return switch (size) {
-            case Dog.XS -> 1;
-            case Dog.S -> 2;
-            case Dog.M -> 3;
-            case Dog.L -> 4;
-            case Dog.XL -> 5;
-            default -> 0;
-        };
-    }
-}
+//package main.java.com.myapp.comparator;
+//
+//import main.java.com.myapp.model.Dog;
+//
+//import java.util.Comparator;
+//
+//public class DogSizeComparator implements Comparator<Dog> {
+//
+//    @Override
+//    public int compare(Dog dog1, Dog dog2) {
+//        int dogSizeCompare = convertSize(dog1.getSize()) - convertSize(dog2.getSize());
+//        int dogAgeCompare = dog1.getAge().compareTo(dog2.getAge());
+//        return (dogSizeCompare == 0) ? dogAgeCompare : dogSizeCompare;
+//    }
+//
+//    private static int convertSize(String size) {
+//        return switch (size) {
+//            case Dog.XS -> 1;
+//            case Dog.S -> 2;
+//            case Dog.M -> 3;
+//            case Dog.L -> 4;
+//            case Dog.XL -> 5;
+//            default -> 0;
+//        };
+//    }
+//}
